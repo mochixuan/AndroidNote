d68c0d4ebdef42c7500636841fa779bdec9bfe9c
wang <1306894924@qq.com>
2017-11-07 20:06:49 +0800

    view xiaomi metion

---
+++ Android-Demo/HenCoder/app/src/main/java/com/wx/hencoder/practice18/view/xmmotion/Model.kt
@@ -0,0 +1,4 @@
+package com.wx.hencoder.practice18.view.xmmotion
+
+data class RingLineModel(val cX:Float,val cY:Float,val radius:Float)
+data class ParticleModel(val cX1:Float,val cY1:Float,val cX2:Float,val cY2:Float)
--- Android-Demo/HenCoder/app/src/main/java/com/wx/hencoder/utils/DisplayUtil.kt
+++ Android-Demo/HenCoder/app/src/main/java/com/wx/hencoder/utils/DisplayUtil.kt
@@ -15,6 +15,14 @@ class DisplayUtil {
             return pxValue/context.resources.displayMetrics.density+0.5f;
         }
 
+        fun spTopx(context: Context,spValue: Float): Float {
+            return context.resources.displayMetrics.scaledDensity*spValue+0.5f
+        }
+
+        fun pxTosp(context: Context,pxValue: Float): Float {
+            return pxValue/context.resources.displayMetrics.scaledDensity+0.5f
+        }
+
         fun getWidth(context: Context): Int {
             return (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.width
         }
