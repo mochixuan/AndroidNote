5ac63b465750c64d79e3328614cb71a310a59df3
wang <1306894924@qq.com>
2018-01-19 11:15:08 +0800

    review popupwindow

--- Android-Demo/OkHttp/app/src/main/java/com/wx/okhttp/MainActivity.java
+++ Android-Demo/OkHttp/app/src/main/java/com/wx/okhttp/MainActivity.java
@@ -1,25 +1,30 @@
 package com.wx.okhttp;
 
+import android.Manifest;
 import android.databinding.ViewDataBinding;
+import android.os.Environment;
 import android.view.View;
 
+import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.wx.okhttp.base.BaseActivity;
 import com.wx.okhttp.databinding.ActivityMainBinding;
 import com.wx.okhttp.tool.OkSimaple1;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.File;
+import java.util.HashMap;
 
-import okhttp3.FormBody;
-import okhttp3.RequestBody;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends BaseActivity {
 
     private ActivityMainBinding binding;
 
-    private static final String WEIXIN_GET_URL = "http://v.juhe.cn/weixin/query?pno=&ps=&dtype=&key=97ccfd2896c2fe7d5662510254995d63";
-    private static final String WEIXIN_POST_URL = "http://v.juhe.cn/weixin/query";
+    private static final String WEIXIN_BASE_URL = "http://v.juhe.cn/weixin/query";
     private static final String BAIDU_URL = "https://www.baidu.com/";
+    private static final String IMG_URL = "http://n.sinaimg.cn/news/transform/w1000h500/20180115/Vdho-fyqrewi3455402.jpg";
+    private static final String FILE_QQ_URL = "http://imtt.dd.qq.com/16891/33757913E2C5D213D1A980EE55FF19C0.apk?fsname=com.tencent.qqmusic_7.9.5.16_763.apk&csr=1bbd";
+    private static final String FILE_UPLOAD_URL1 = "https://api.github.com/markdown/raw";
+    private static final String FILE_UPLOAD_URL2 = "https://api.github.com/markdown/raw";
 
     @Override
     public int getLayoutId() {
@@ -33,39 +38,55 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initData() {
+        initPermission();
+
+        final HashMap<String,String> weixinParams = new HashMap<>();
+        weixinParams.put("pno","1");
+        weixinParams.put("ps","10");
+        weixinParams.put("dtype","json");
+        weixinParams.put("key","97ccfd2896c2fe7d5662510254995d63");
+
         binding.btnGet1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                OkSimaple1.getInstance().get(WEIXIN_GET_URL,0);
-                OkSimaple1.getInstance().get(BAIDU_URL,1);
+                OkSimaple1.getInstance().get(WEIXIN_BASE_URL,weixinParams,0);
+                OkSimaple1.getInstance().get(BAIDU_URL,null,1);
             }
         });
         binding.btnPost1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("pno",1);
-                    jsonObject.put("ps",10);
-                    jsonObject.put("dtype","json");
-                    jsonObject.put("key","97ccfd2896c2fe7d5662510254995d63");
-                    OkSimaple1.getInstance().postJson(WEIXIN_POST_URL,0,jsonObject.toString());
-
-
-                    RequestBody requestBody = new FormBody.Builder()
-                            .add("pno", String.valueOf(1))
-                            .add("ps", String.valueOf(10))
-                            .add("dtype","json")
-                            .add("key","97ccfd2896c2fe7d5662510254995d63")
-                            .build();
-                    OkSimaple1.getInstance().postForm(WEIXIN_POST_URL,1,requestBody);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
+                OkSimaple1.getInstance().postJson(WEIXIN_BASE_URL,weixinParams,0);
+                OkSimaple1.getInstance().postForm(WEIXIN_BASE_URL,weixinParams,1);
             }
         });
+        binding.btnDownfile1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //OkSimaple1.getInstance().downFile(IMG_URL, Environment.getExternalStorageDirectory()+"/okttpsimaple/test.png",0);
+                OkSimaple1.getInstance().downFile(FILE_QQ_URL, Environment.getExternalStorageDirectory()+"/okttpsimaple/qq.apk",1);
+            }
+        });
+        binding.btnUpload1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String,Object> map = new HashMap<>();
+                map.put("image",new File(Environment.getExternalStorageDirectory()+"/okttpsimaple/test.png"));
+                OkSimaple1.getInstance().uploadFile(FILE_UPLOAD_URL1,map,0);
+            }
+        });
+
+    }
+
+    private void initPermission() {
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        if (!aBoolean) finish();
+                    }
+                });
     }
 
 }
--- Android-Demo/OkHttp/app/src/main/java/com/wx/okhttp/tool/OkSimaple1.java
+++ Android-Demo/OkHttp/app/src/main/java/com/wx/okhttp/tool/OkSimaple1.java
@@ -2,11 +2,19 @@ package com.wx.okhttp.tool;
 
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -19,6 +27,7 @@ import okhttp3.Response;
 public class OkSimaple1 {
 
     private static OkSimaple1 INSTANCE;
+    private OkHttpClient mOkHttpClient;
 
     private final String TAG = this.getClass().getSimpleName();
 
@@ -33,63 +42,206 @@ public class OkSimaple1 {
         return INSTANCE;
     }
 
-    public void get(String url, final int key) {
-        OkHttpClient httpClient = new OkHttpClient();
-        Request request = new Request.Builder().url(url).build();
-        httpClient.newCall(request).enqueue(new Callback() {
+    private OkSimaple1() {
+        //mOkHttpClient = new OkHttpClient();
+        mOkHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)  //设置超时时间
+                .readTimeout(10, TimeUnit.SECONDS)     //设置读取超时时间
+                .writeTimeout(10, TimeUnit.SECONDS)    //设置写入超时时间
+                .build();
+    }
+
+    private Request.Builder mRequestBuilder;
+    private Request.Builder getRequestBuilder() {
+        if (mRequestBuilder == null) {
+            mRequestBuilder = new Request.Builder()
+                    .addHeader("Connection", "keep-alive");
+                    //.addHeader("platform", "2")
+                    //.addHeader("phoneModel", Build.MODEL)
+                    //.addHeader("systemVersion", Build.VERSION.RELEASE)
+                    //.addHeader("appVersion", "3.2.0");
+        }
+        return mRequestBuilder;
+    }
+
+    public void get(String baseUrl, HashMap<String,String> params,final int index) {
+        StringBuffer tempParams = new StringBuffer();
+        tempParams.append(baseUrl);
+        if (params != null) {
+            tempParams.append("?");
+            int pos = 0;
+            for (String key : params.keySet()) {
+                if (pos > 0) {
+                    tempParams.append("&");
+                }
+                pos = 1;
+                try {
+                    tempParams.append(String.format("%s=%s",key, URLEncoder.encode(params.get(key),"UTF-8")));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        Request request = getRequestBuilder().url(tempParams.toString()).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.w(TAG,key+" onFailure: ");
+                Log.w(TAG,index+" onFailure: ");
             }
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.d(TAG,key+" onResponse: "+response.body().string());
+                Log.d(TAG,index+" onResponse: "+response.body().string());
             }
         });
     }
 
     //传入参数为json
-    public void postJson(String url, final int key, String json) {
-        OkHttpClient httpClient = new OkHttpClient();
+    public void postJson(String baseUrl, HashMap<String,String> params , final int index) {
+        StringBuffer tempParams = new StringBuffer();
+        tempParams.append(baseUrl);
+        tempParams.append("{");
+        for (String key : params.keySet()) {
+            try {
+                tempParams.append(String.format("%s:%s",key, URLEncoder.encode(params.get(key),"UTF-8")));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        tempParams.append("}");
         RequestBody requestBody = RequestBody.create(
                 MediaType.parse("application/json; charset=utf-8"),
-                json);
-        Request request = new Request.Builder().url(url).post(requestBody).build();
-        httpClient.newCall(request).enqueue(new Callback() {
+                tempParams.toString());
+        Request request = getRequestBuilder().url(baseUrl).post(requestBody).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.w(TAG,key+" onFailure: ");
+                Log.w(TAG,index+" onFailure: ");
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.d(TAG,key+" onResponse: "+response.body().string());
+                Log.d(TAG,index+" onResponse: "+response.body().string());
             }
         });
     }
 
     //表单形式
-    public void postForm(String url, final int key, RequestBody body) {
-        OkHttpClient httpClient = new OkHttpClient();
-        Request request = new Request.Builder().url(url).post(body).build();
-        httpClient.newCall(request).enqueue(new Callback() {
+    public void postForm(String url, HashMap<String,String> params, final int index) {
+        FormBody.Builder builder = new FormBody.Builder();
+        for (String key : params.keySet()) {
+            try {
+                builder.add(key,URLEncoder.encode(params.get(key),"UTF-8"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Request request = getRequestBuilder().url(url).post(builder.build()).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.w(TAG,key+" onFailure: ");
+                Log.w(TAG,index+" onFailure: ");
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.d(TAG,key+" onResponse: "+response.body().string());
+                Log.d(TAG,index+" onResponse: "+response.body().string());
             }
         });
     }
 
     //上传文件
-    public void postFile() {
-        OkHttpClient httpClient = new OkHttpClient();
+    public void uploadFile(String url, HashMap<String,Object> params, final int index) {
+        MultipartBody.Builder builder = new MultipartBody.Builder();
+        builder.setType(MultipartBody.FORM);
+        for (String key : params.keySet()) {
+            try {
+                Object object = params.get(key);
+                if (!(object instanceof File)) {
+                    builder.addFormDataPart(key, (String) params.get(key));
+                } else {
+                    File file = (File) object;
+                    RequestBody requestBody = RequestBody.create(
+                            MediaType.parse("text/x-markdown; charset=utf-8"),//和服务器要相同
+                            //MediaType.parse("application/octet-stream")
+                            file
+                    );
+                    builder.addFormDataPart(key,file.getName(),requestBody);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Request request = getRequestBuilder().post(builder.build()).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.w(TAG,index+" onFailure: ");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG,index+" onResponse: "+response.body().string());
+            }
+        });
     }
 
+    //文件下载
+    public void downFile(String fileUrl,String filePath,final int index) {
+        final File file = new File(filePath);
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+        }
+        Request request = getRequestBuilder().url(fileUrl).build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.w(TAG,index+" onFailure: ");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                saveFile(response,file);
+            }
+        });
+    }
+
+    private void saveFile(Response response,File file) {
+        if (response.isSuccessful()) {
+            FileOutputStream fos = null;
+            InputStream is = null;
+            try {
+                fos = new FileOutputStream(file);
+                long total = response.body().contentLength();
+                long current = 0;
+                is = response.body().byteStream();
+                int len = 0;
+                byte[] buf = new byte[2048];
+                while ((len = is.read(buf))!= -1) {
+                    current += len;
+                    fos.write(buf,0,len);
+                    Log.d(TAG,"=======file progress: "+(current*100f/total));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (is != null) {
+                        is.close();
+                    }
+                    if (fos != null) {
+                        fos.flush();
+                        fos.close();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     public OkHttpClient.Builder getBuilder() {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
